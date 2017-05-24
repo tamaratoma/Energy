517134b341c5b4e139820f0577cabf71359f0b27
tamaratoma <tamaratoma@hotmail.de>
2017-05-24 20:00:25 +0200

    completed Site

--- src/fowler/energybilling/Site.java
+++ src/fowler/energybilling/Site.java
@@ -1,41 +1,57 @@
 package fowler.energybilling;
 
-
-/*
-*  Super class for the different site classes.
-*/
+import java.util.Calendar;
+import java.util.Date;
 
 public class Site {
 
+	protected Zone _zone;
+	protected Reading[] readings = new Reading[1000];
 
-    protected Zone zone;
-    protected Reading[] readings = new Reading[1000];
-    
-    Site(Zone zone){
-          this.zone = zone;
-    }
-    
-    /*
-    * New addReading method, pulled up from subclasses.
-    * Adds the new reading into the first free spot in the array.
-    *  
-     */
-    public void addReading(Reading newReading){
-          
-          readings[firstUnusedReadingsIndex()] = newReading;
+	Site(Zone zone) {
+		_zone = zone;
+	}
 
-    }
-    
-    /*
-    *  Finding the first non-null index of the array.
-    */
-    private int firstUnusedReadingsIndex(){
-          
-          int i = 0;
-          while(readings[i] != null) i++;
-          
-          return i;
-          
-    }
-    
+	public void addReading(Reading newReading) {
+
+		readings[firstUnusedReadingsIndex()] = newReading;
+	}
+
+	private int firstUnusedReadingsIndex() {
+		int i = 0;
+		while (readings[i] != null) {
+			i++;
+		}
+		return i;
+	}
+
+	private Dollars charge(int fullUsage, Calendar start, Calendar end) {
+		return new Dollars();
+	}
+
+	public Dollars charge() {
+		Calendar end = lastReading().date();
+		Calendar start = nextDay(previousReading().date());
+		return charge(lastUsage(), start, end);
+	}
+
+	private Calendar nextDay(Calendar arg) {
+		// foreign method - should be in Date
+		Calendar result = Calendar.getInstance();
+		result.setTime(arg.getTime());
+		result.add(Calendar.DATE, 1);
+		return result;
+	}
+
+	private int lastUsage() {
+		return lastReading().amount() - previousReading().amount();
+	};
+
+	private Reading previousReading() {
+		return readings[firstUnusedReadingsIndex() - 2];
+	}
+
+	private Reading lastReading() {
+		return readings[firstUnusedReadingsIndex() - 1];
+	};
 }
