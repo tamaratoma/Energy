e4790c2760dbb7a2af4dd87cf350dd73cd9e28b3
tamaratoma <tamaratoma@hotmail.de>
2017-05-25 15:25:09 +0200

    Add files via upload

---
+++ src/fowler/energybilling/MfDate.java
@@ -0,0 +1,82 @@
+package fowler.energybilling;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.lang.Cloneable;
+
+public class MfDate implements Cloneable {
+
+	/*
+	 * public MfDate(Date arg) { super(arg.getTime()); }
+	 * 
+	 * public MfDate(String dateString) { super(dateString); };
+	 */
+
+	public Object clone() {
+		try {
+			return super.clone();
+		} catch (CloneNotSupportedException e) {
+			// should not happen
+			throw new InternalError(e.toString());
+		}
+	}
+
+	public MfDate copy() {
+		return (MfDate) clone();
+	}
+	
+	
+	public MfDate nextDay() {
+		MfDate result = copy();
+		result.setDate(result.getDate() + 1);
+		return result;
+	}
+
+	public static MfDate earliest(MfDate arg1, MfDate arg2) {
+		return (arg1.before(arg2)) ? arg1 : arg2;
+	}
+
+	public static MfDate latest(MfDate arg1, MfDate arg2) {
+		return (arg1.after(arg2)) ? arg1 : arg2;
+	}
+
+	int dayOfYear() {
+		int result = daysToStartOfMonth() + getDate();
+		if (isLeapYear() & this.after(new Date(getYear(), 1, 29)))
+			result++;
+		return result;
+	}
+
+	boolean isLeapYear() {
+		return (getYear() % 4 == 0) && ((getYear() % 100 != 0) || ((getYear() + 1900) % 400 == 0));
+	}
+
+	private int daysToStartOfMonth() {
+		int[] monthNumbers = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 };
+		return monthNumbers[getMonth()];
+	}
+
+	private void requireSameYear(MfDate arg) {
+		if (getYear() != arg.getYear())
+			throw new IllegalArgumentException("Arguments must be in same year");
+	}
+
+	public int minus(MfDate arg) {
+		return (getYear() == arg.getYear()) ? dayOfYear() - arg.dayOfYear() : // shortcut
+				daysSince1901() - arg.daysSince1901();
+	}
+
+	public int daysSince1901() {
+		if (getYear() < 1)
+			throw new IllegalArgumentException();
+		int result;
+		int yearIndex = getYear() - 1;
+		result = yearIndex * 365;
+		result += yearIndex / 4; // ordinary leap years
+		result += (yearIndex + 300) / 400; // leap centuries
+		result -= yearIndex / 100; // non-leap centuries
+		result += dayOfYear() - 1;
+		return result;
+	}
+
+}
---
+++ src/fowler/energybilling/RateTable.java
@@ -0,0 +1,12 @@
+package fowler.energybilling;
+
+public class RateTable {
+	public RateTable(double[] table) {
+		_table = table;
+	}
+
+	private double[] _table;
+	
+	
+	//bla
+}
